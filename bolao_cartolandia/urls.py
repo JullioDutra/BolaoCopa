5ffe34e5d3d68a47e5db7f59a8433e3824124474
Jullio Cesar <134459685+JullioDutra@users.noreply.github.com>
2026-05-18 20:00:25 -0300

    Import auth_views from django.contrib.auth

--- bolao_cartolandia/urls.py
+++ bolao_cartolandia/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from core.views import dashboard_view
 from accounts import views as accounts_views
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 

4024c0e40b9f450d2028cdcbed6c9e65fde49abe
Jullio Cesar <134459685+JullioDutra@users.noreply.github.com>
2026-06-14 00:09:46 -0300

    ajuste

--- minijogo/urls.py
+++ minijogo/urls.py
@@ -4,12 +4,14 @@ from . import views
 app_name = 'minijogo'
 
 urlpatterns = [
-
-    path('api/draft/escolher/', views.api_escolher_carta, name='api_escolher_carta'),
-    path('api/partida/acao/', views.api_enviar_acao, name='api_enviar_acao'),
-    path('api/partida/<int:partida_id>/status/', views.api_status_partida, name='api_status_partida'),
+    # --- Telas Visuais ---
     path('draft/', views.tela_draft, name='tela_draft'),
     path('lobby/', views.lobby_batalha, name='lobby'),
     path('partida/<int:partida_id>/', views.tela_jogo, name='tela_jogo'),
     path('convite/<int:partida_id>/', views.aceitar_convite, name='aceitar_convite'),
+    
+    # --- APIs (Chamadas AJAX em segundo plano) ---
+    path('api/draft/escolher/', views.api_escolher_carta, name='api_escolher_carta'),
+    path('api/partida/acao/', views.api_enviar_acao, name='api_enviar_acao'),
+    path('api/partida/<int:partida_id>/status/', views.api_status_partida, name='api_status_partida'),
 ]

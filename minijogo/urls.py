242151d13dad842e42400a615c8542e2dc36e963
Jullio Cesar <134459685+JullioDutra@users.noreply.github.com>
2026-06-15 17:21:38 -0300

    Add route to cancel lobby in urls.py

--- minijogo/urls.py
+++ minijogo/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('convite/<int:partida_id>/', views.aceitar_convite, name='aceitar_convite'),
     path('api/partida/desistir/', views.api_desistir, name='api_desistir'),
     path('ranking/', views.ranking_x1, name='ranking_x1'),
+    path('partida/<int:partida_id>/cancelar/', views.cancelar_lobby, name='cancelar_lobby'),
 
     
     # --- APIs (Chamadas AJAX em segundo plano) ---

446bca632dffd53bb8159031c44f6aa823323470
Jullio Cesar <134459685+JullioDutra@users.noreply.github.com>
2026-07-16 08:59:21 -0300

    Add ResultadosFinaisForm to handle final results

--- palpites/forms.py
+++ palpites/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Palpite
+from .models import Clube, Temporada,Palpite
 
 class PalpiteForm(forms.ModelForm):
     class Meta:
@@ -23,3 +23,27 @@ class PalpiteForm(forms.ModelForm):
             raise forms.ValidationError("Tempo esgotado! A bola já rolou e os palpites para este jogo estão encerrados.")
             
         return cleaned_data
+
+
+class ResultadosFinaisForm(forms.Form):
+    temporada_ano = forms.ModelChoiceField(
+        queryset=Temporada.objects.filter(ativa=True),
+        label="Temporada Ativa",
+        empty_label="Selecione a Temporada"
+    )
+    
+    # G4
+    campeao_br = forms.ModelChoiceField(queryset=Clube.objects.all(), label="1º Lugar (Campeão BR)")
+    vice_br = forms.ModelChoiceField(queryset=Clube.objects.all(), label="2º Lugar")
+    terceiro_br = forms.ModelChoiceField(queryset=Clube.objects.all(), label="3º Lugar")
+    quarto_br = forms.ModelChoiceField(queryset=Clube.objects.all(), label="4º Lugar")
+    
+    # Z4
+    pos_17 = forms.ModelChoiceField(queryset=Clube.objects.all(), label="17º Lugar")
+    pos_18 = forms.ModelChoiceField(queryset=Clube.objects.all(), label="18º Lugar")
+    pos_19 = forms.ModelChoiceField(queryset=Clube.objects.all(), label="19º Lugar")
+    pos_20 = forms.ModelChoiceField(queryset=Clube.objects.all(), label="20º Lugar")
+    
+    # Outros Campeões
+    campeao_europa = forms.ModelChoiceField(queryset=Clube.objects.all(), label="Campeão Europeu (Champions)")
+    campeao_cdb = forms.ModelChoiceField(queryset=Clube.objects.all(), label="Campeão Copa do Brasil")

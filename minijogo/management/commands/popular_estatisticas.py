719cc9d13b92aa2a4f6482d5f3c3ac219f1d16ce
Jullio Cesar <134459685+JullioDutra@users.noreply.github.com>
2026-08-04 10:16:17 -0300

    Update player statistics data in management command

--- minijogo/management/commands/popular_estatisticas.py
+++ minijogo/management/commands/popular_estatisticas.py
@@ -2,45 +2,43 @@ from django.core.management.base import BaseCommand
 from minijogo.models import EstatisticaJogador
 
 class Command(BaseCommand):
-    help = 'Popula o banco de dados com cartas de Lendas do Futebol Mundial para o minijogo.'
+    help = 'Popula o banco de dados com cartas de jogadores medianos, folclóricos e operários com ótimos números.'
 
     def handle(self, *args, **kwargs):
         jogadores_data = [
             # ATACANTES
-            {"nome": "Pelé", "clube": "Santos", "posicao": "ATA", "gols": 767, "assistencias": 340, "cartoes_amarelos": 15, "cartoes_vermelhos": 0, "jogos": 831},
-            {"nome": "Ronaldo Fenômeno", "clube": "Real Madrid", "posicao": "ATA", "gols": 414, "assistencias": 104, "cartoes_amarelos": 35, "cartoes_vermelhos": 2, "jogos": 616},
-            {"nome": "Romário", "clube": "Vasco da Gama", "posicao": "ATA", "gols": 772, "assistencias": 150, "cartoes_amarelos": 80, "cartoes_vermelhos": 10, "jogos": 994},
-            {"nome": "Ferenc Puskás", "clube": "Real Madrid", "posicao": "ATA", "gols": 746, "assistencias": 120, "cartoes_amarelos": 10, "cartoes_vermelhos": 1, "jogos": 754},
-            {"nome": "Johan Cruyff", "clube": "Ajax", "posicao": "ATA", "gols": 405, "assistencias": 180, "cartoes_amarelos": 35, "cartoes_vermelhos": 3, "jogos": 716},
-            {"nome": "Garrincha", "clube": "Botafogo", "posicao": "ATA", "gols": 245, "assistencias": 110, "cartoes_amarelos": 12, "cartoes_vermelhos": 1, "jogos": 614},
+            {"nome": "Obina", "clube": "Flamengo", "posicao": "ATA", "gols": 118, "assistencias": 25, "cartoes_amarelos": 45, "cartoes_vermelhos": 4, "jogos": 412},
+            {"nome": "Deyverson", "clube": "Atlético-MG", "posicao": "ATA", "gols": 92, "assistencias": 21, "cartoes_amarelos": 75, "cartoes_vermelhos": 8, "jogos": 345},
+            {"nome": "Hernane Brocador", "clube": "Flamengo", "posicao": "ATA", "gols": 145, "assistencias": 28, "cartoes_amarelos": 30, "cartoes_vermelhos": 2, "jogos": 430},
+            {"nome": "Borges", "clube": "Santos", "posicao": "ATA", "gols": 182, "assistencias": 35, "cartoes_amarelos": 65, "cartoes_vermelhos": 5, "jogos": 485},
+            {"nome": "Alecsandro (Alecgol)", "clube": "Vasco da Gama", "posicao": "ATA", "gols": 178, "assistencias": 42, "cartoes_amarelos": 55, "cartoes_vermelhos": 3, "jogos": 510},
+            {"nome": "Souza Caveirão", "clube": "Bahia", "posicao": "ATA", "gols": 135, "assistencias": 20, "cartoes_amarelos": 70, "cartoes_vermelhos": 9, "jogos": 420},
             
             # MEIO-CAMPISTAS
-            {"nome": "Diego Maradona", "clube": "Napoli", "posicao": "MEI", "gols": 345, "assistencias": 240, "cartoes_amarelos": 50, "cartoes_vermelhos": 4, "jogos": 680},
-            {"nome": "Zinedine Zidane", "clube": "Real Madrid", "posicao": "MEI", "gols": 156, "assistencias": 119, "cartoes_amarelos": 65, "cartoes_vermelhos": 14, "jogos": 795},
-            {"nome": "Ronaldinho Gaúcho", "clube": "Barcelona", "posicao": "MEI", "gols": 299, "assistencias": 214, "cartoes_amarelos": 70, "cartoes_vermelhos": 7, "jogos": 719},
-            {"nome": "Zico", "clube": "Flamengo", "posicao": "MEI", "gols": 525, "assistencias": 250, "cartoes_amarelos": 40, "cartoes_vermelhos": 2, "jogos": 769},
-            {"nome": "Andrés Iniesta", "clube": "Barcelona", "posicao": "MEI", "gols": 93, "assistencias": 190, "cartoes_amarelos": 65, "cartoes_vermelhos": 0, "jogos": 1016},
-            {"nome": "Michel Platini", "clube": "Juventus", "posicao": "MEI", "gols": 353, "assistencias": 150, "cartoes_amarelos": 30, "cartoes_vermelhos": 1, "jogos": 652},
+            {"nome": "Diego Souza", "clube": "Grêmio", "posicao": "MEI", "gols": 205, "assistencias": 115, "cartoes_amarelos": 120, "cartoes_vermelhos": 11, "jogos": 780},
+            {"nome": "Yago Pikachu", "clube": "Fortaleza", "posicao": "MEI", "gols": 135, "assistencias": 80, "cartoes_amarelos": 75, "cartoes_vermelhos": 4, "jogos": 590},
+            {"nome": "Cícero", "clube": "Fluminense", "posicao": "MEI", "gols": 110, "assistencias": 55, "cartoes_amarelos": 85, "cartoes_vermelhos": 3, "jogos": 620},
+            {"nome": "Márcio Araújo", "clube": "Flamengo", "posicao": "MEI", "gols": 12, "assistencias": 22, "cartoes_amarelos": 105, "cartoes_vermelhos": 2, "jogos": 680},
+            {"nome": "Thiago Galhardo", "clube": "Internacional", "posicao": "MEI", "gols": 95, "assistencias": 45, "cartoes_amarelos": 75, "cartoes_vermelhos": 6, "jogos": 410},
 
             # LATERAIS
-            {"nome": "Roberto Carlos", "clube": "Real Madrid", "posicao": "LAT", "gols": 113, "assistencias": 125, "cartoes_amarelos": 130, "cartoes_vermelhos": 10, "jogos": 945},
-            {"nome": "Cafu", "clube": "Milan", "posicao": "LAT", "gols": 25, "assistencias": 100, "cartoes_amarelos": 110, "cartoes_vermelhos": 6, "jogos": 800},
-            {"nome": "Carlos Alberto Torres", "clube": "Santos", "posicao": "LAT", "gols": 60, "assistencias": 75, "cartoes_amarelos": 40, "cartoes_vermelhos": 3, "jogos": 740},
-            {"nome": "Philipp Lahm", "clube": "Bayern Munich", "posicao": "LAT", "gols": 22, "assistencias": 77, "cartoes_amarelos": 48, "cartoes_vermelhos": 0, "jogos": 765},
+            {"nome": "Reinaldo (Kingnaldo)", "clube": "São Paulo", "posicao": "LAT", "gols": 38, "assistencias": 68, "cartoes_amarelos": 115, "cartoes_vermelhos": 5, "jogos": 540},
+            {"nome": "Rodinei", "clube": "Flamengo", "posicao": "LAT", "gols": 10, "assistencias": 45, "cartoes_amarelos": 85, "cartoes_vermelhos": 4, "jogos": 420},
+            {"nome": "Nino Paraíba", "clube": "Bahia", "posicao": "LAT", "gols": 15, "assistencias": 40, "cartoes_amarelos": 135, "cartoes_vermelhos": 8, "jogos": 650},
+            {"nome": "Apodi", "clube": "Chapecoense", "posicao": "LAT", "gols": 30, "assistencias": 25, "cartoes_amarelos": 90, "cartoes_vermelhos": 6, "jogos": 510},
+            {"nome": "Pará", "clube": "Santos", "posicao": "LAT", "gols": 4, "assistencias": 35, "cartoes_amarelos": 110, "cartoes_vermelhos": 7, "jogos": 580},
 
             # ZAGUEIROS
-            {"nome": "Paolo Maldini", "clube": "Milan", "posicao": "ZAG", "gols": 33, "assistencias": 43, "cartoes_amarelos": 87, "cartoes_vermelhos": 3, "jogos": 902},
-            {"nome": "Franz Beckenbauer", "clube": "Bayern Munich", "posicao": "ZAG", "gols": 109, "assistencias": 75, "cartoes_amarelos": 40, "cartoes_vermelhos": 0, "jogos": 820},
-            {"nome": "Fabio Cannavaro", "clube": "Juventus", "posicao": "ZAG", "gols": 16, "assistencias": 10, "cartoes_amarelos": 115, "cartoes_vermelhos": 8, "jogos": 690},
-            {"nome": "Franco Baresi", "clube": "Milan", "posicao": "ZAG", "gols": 33, "assistencias": 24, "cartoes_amarelos": 50, "cartoes_vermelhos": 4, "jogos": 719},
-            {"nome": "Carles Puyol", "clube": "Barcelona", "posicao": "ZAG", "gols": 24, "assistencias": 16, "cartoes_amarelos": 140, "cartoes_vermelhos": 4, "jogos": 682},
+            {"nome": "Réver", "clube": "Atlético-MG", "posicao": "ZAG", "gols": 42, "assistencias": 12, "cartoes_amarelos": 85, "cartoes_vermelhos": 4, "jogos": 550},
+            {"nome": "Antônio Carlos", "clube": "Botafogo", "posicao": "ZAG", "gols": 38, "assistencias": 8, "cartoes_amarelos": 95, "cartoes_vermelhos": 6, "jogos": 480},
+            {"nome": "Rodrigo", "clube": "Vasco da Gama", "posicao": "ZAG", "gols": 35, "assistencias": 10, "cartoes_amarelos": 140, "cartoes_vermelhos": 14, "jogos": 520},
+            {"nome": "Domingos", "clube": "Santos", "posicao": "ZAG", "gols": 5, "assistencias": 3, "cartoes_amarelos": 160, "cartoes_vermelhos": 18, "jogos": 390},
             
             # GOLEIROS
-            {"nome": "Lev Yashin", "clube": "Dynamo Moscow", "posicao": "GOL", "gols": 0, "assistencias": 0, "cartoes_amarelos": 5, "cartoes_vermelhos": 0, "jogos": 326},
-            {"nome": "Gianluigi Buffon", "clube": "Juventus", "posicao": "GOL", "gols": 0, "assistencias": 1, "cartoes_amarelos": 40, "cartoes_vermelhos": 5, "jogos": 1151},
-            {"nome": "Oliver Kahn", "clube": "Bayern Munich", "posicao": "GOL", "gols": 0, "assistencias": 12, "cartoes_amarelos": 45, "cartoes_vermelhos": 3, "jogos": 780},
-            {"nome": "Rogério Ceni", "clube": "São Paulo", "posicao": "GOL", "gols": 131, "assistencias": 0, "cartoes_amarelos": 60, "cartoes_vermelhos": 4, "jogos": 1237},
-            {"nome": "Gordon Banks", "clube": "Leicester City", "posicao": "GOL", "gols": 0, "assistencias": 0, "cartoes_amarelos": 2, "cartoes_vermelhos": 0, "jogos": 678},
+            {"nome": "Márcio", "clube": "Atlético-GO", "posicao": "GOL", "gols": 34, "assistencias": 0, "cartoes_amarelos": 45, "cartoes_vermelhos": 3, "jogos": 530},
+            {"nome": "Fábio Costa", "clube": "Santos", "posicao": "GOL", "gols": 0, "assistencias": 2, "cartoes_amarelos": 95, "cartoes_vermelhos": 8, "jogos": 580},
+            {"nome": "Denis", "clube": "São Paulo", "posicao": "GOL", "gols": 0, "assistencias": 1, "cartoes_amarelos": 25, "cartoes_vermelhos": 2, "jogos": 250},
+            {"nome": "Vanderlei", "clube": "Santos", "posicao": "GOL", "gols": 0, "assistencias": 1, "cartoes_amarelos": 30, "cartoes_vermelhos": 1, "jogos": 620},
         ]
 
         cadastrados = 0
@@ -65,4 +63,4 @@ class Command(BaseCommand):
             else:
                 atualizados += 1
 
-        self.stdout.write(self.style.SUCCESS(f'Sucesso! {cadastrados} lendas criadas e {atualizados} atualizadas.'))
+        self.stdout.write(self.style.SUCCESS(f'Sucesso! {cadastrados} jogadores folclóricos criados e {atualizados} atualizados.'))
